3bbd357073ebcc658d35ff881fbc8182a1478c4d
Gasper Lukman <gaspercino.lukman@gmail.com>
2024-05-23 11:36:15 +0200

    [RUG-19]
    - Changed backend url

--- app/backendapi/src/main/java/eu/virtusdevelops/backendapi/Api.kt
+++ app/backendapi/src/main/java/eu/virtusdevelops/backendapi/Api.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object Api {
 
     // todo: change this
-    private const val BASE_URL = "http://192.168.1.69:8080/api/v1/"
+    private const val BASE_URL = "https://rug-be.virtus.lol/api/v1/"
 
 
     val api: ApiRequest by lazy {
